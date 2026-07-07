de42f75916dbde8848adb6f0e842b9ba47579e60
mohammad <msalhab270@gmail.com>
2026-07-07 12:19:45 +0300

    code update

--- proj1.py
+++ proj1.py
@@ -1,4 +1,8 @@
+from os import name
+
+
 student={}
+average_grades={}
 
 def add_grades(student, name, grade):
     if name not in student:
@@ -10,25 +14,58 @@ add_grades(student, "mohammad", 70)
 add_grades(student, "mohammad", 90)
 add_grades(student, "adam", 100)
 add_grades(student, "adam", 80)
-
-
+add_grades(student, "baha", 60)
+add_grades(student, "baha", 55)
 print(student)
 
 def calculate_average(student, name):
-    
-    for name in student:
-             print(sum(student[name]) / len(student[name]))
-    
+    try:
+        print(sum(student[name]) / len(student[name]))
+    except (KeyError, ZeroDivisionError):
+        print("Student not found or no grades available.")
 
 
-calculate_average(student, "mohammad")
+calculate_average(student, "zoozo")
 
-def print3students(student):
-    if len(student) >= 3:
-      print(f"The first three students are:{list(student.keys())[:3]}")
 
-      print3students(student)
+
+
+
+
       
 
+def printtopstudent(student, average_grades):
+    for name in student:
+        average_grades[name] = sum(student[name]) / len(student[name])
+    top_student = max(average_grades, key=average_grades.get)
+    print(f"{top_student} is the top student with an average grade of {average_grades[top_student]}")
 
-            
+printtopstudent(student, average_grades)
+
+
+
+
+def remove_student(student, name):
+    if name in student:
+        del student[name]
+        print(f"{name} has been removed from the student list.")
+    else:
+        print(f"{name} is not in the student list.")
+
+
+
+
+
+remove_student(student, "adam")
+print(student)
+
+
+
+
+
+
+
+
+
+      
+        
