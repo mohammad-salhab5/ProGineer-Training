b2e4dd2c167a7dfee81730b3e136d9767f42b234
Adam Abughayyada <202304046@bethlehem.edu>
2026-07-08 12:18:31 +0300

    new functions

--- aaa.py
+++ aaa.py
@@ -3,9 +3,13 @@ students = {}
 def add_grades(students, name, grade):
     if name not in students:
         students[name] = []
-    students[name].append(grade)
 
-add_grades(students, "John", 85)
+    if 0 <= grade <= 100:
+        students[name].append(grade)
+    else:
+        print("Grade must be between 0 and 100")
+
+add_grades(students, "John", 155)
 add_grades(students, "John", 85)
 add_grades(students, "Alice", 90)
 add_grades(students, "Alice", 95)
@@ -15,12 +19,15 @@ add_grades(students, "Bob", 92)
 print(students)
 
 def calculate_average(students, name):
-    if name in students:
-        grades = students[name]
-        average = sum(grades) / len(grades)
-        return average
-    else:
+    if name not in students:
         return None
+
+    grades = students[name]
+
+    if not grades:
+        return None 
+
+    return sum(grades) / len(grades)
 
 print(calculate_average(students, "John"))
 print(calculate_average(students, "Alice"))
@@ -50,3 +57,98 @@ def top_student(students):
     return top_name, top_average
 
 print(top_student(students))
+#we can do it on calculate_average funcation
+def average_grade(students, name):
+    if name not in students:
+        return "No student found with that name."
+       
+    grades = students[name]
+    return sum(grades) / len(grades)
+
+avg=average_grade(students, "mohammad")
+print(avg)
+avg2=average_grade(students, "John")
+print(avg2)
+
+def class_average(students):
+    total_grades=0
+    total_students=0
+    for grades in students.values():
+        total_grades +=sum(grades)
+        total_students +=len(grades)
+    if total_students==0:
+        return "there are no students in the class."
+    
+    return total_grades/total_students
+
+print(class_average(students))
+
+def student_report(students, name):
+    if name not in students:
+        return "No student found with that name."
+
+    report = "{name} : grades= {grades} , average={average}".format(
+        name=name,
+        grades=students[name],
+        average=average_grade(students, name)
+    )
+    return report
+
+   
+
+
+repo = student_report(students, "Alice")
+print(repo)
+
+
+
+def sort_average(students):
+    averages = {}
+
+    for name in students:
+        averages[name] = average_grade(students, name)
+
+    sorted_averages = sorted(averages.items(), key=lambda x: x[1], reverse=True)
+
+    names = []
+    for name, average in sorted_averages:
+        names.append(name)
+
+    return names
+
+
+sorted_students = sort_average(students)
+print(sorted_students)
+
+
+
+
+def letter_grade(score):
+    if score < 0 or score > 100:
+        return "Invalid score. Please enter a score between 0 and 100."
+    if score >= 90:
+        return "A"
+    elif score >= 80 and score <= 89:
+        return "B"
+    elif score >= 70 and score <= 79:
+        return "C" 
+    elif score >=60 and score <= 69:
+        return "D"
+    else:
+        return "F"
+    
+
+    
+    
+    
+letter = letter_grade(87.66666666666667)
+print(letter)
+
+letter2 = letter_grade(105)
+print(letter2)
+
+letter3 = letter_grade(-5)
+print(letter3)
+
+letter4 = letter_grade(22)
+print(letter4)
